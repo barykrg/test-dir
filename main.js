f49ae7cb75f4dd45a878dc6b572edbaf8e35b1f3
barykrg <barykrg@gmail.com>
2021-04-07 10:00:24 +0530

    blur

--- main.js
+++ main.js
@@ -67,18 +67,15 @@ function removeFromOtherRow(div)
         document.getElementById('option-byo-openvino').classList.remove('selected');
         document.getElementById('option-byo-none').classList.add('selected');
     }
-    if(div.id.startsWith('option-byo'))
+    if(document.getElementById('option-byo-tfov').classList.contains('selected'))
     {
-        if(div.id=='option-byo-tfov' && div.classList.contains('selected'))
-        {
-            document.getElementById('option-hddl-yes').classList.add('blur');
-            document.getElementById('option-hddl-no').classList.add('blur');
-        }
-        else
-        {
-            document.getElementById('option-hddl-yes').classList.remove('blur');
-            document.getElementById('option-hddl-no').classList.remove('blur');
-        }
+        document.getElementById('option-hddl-no').classList.add('blur');
+        document.getElementById('option-hddl-yes').classList.add('blur');
+    }
+    else
+    {
+        document.getElementById('option-hddl-no').classList.remove('blur');
+        document.getElementById('option-hddl-yes').classList.remove('blur');
     }
 
 }
