c7e30bc987e55af4740cf62f51d8cc51e8068bb4
barykrg <barykrg@gmail.com>
2021-04-12 09:17:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -72,6 +72,7 @@ function removeFromOtherRow(div)
     if(div.id=='option-distro-pip' && document.getElementById('option-byo-openvino').classList.contains('selected')&&document.getElementById('option-hddl-no').classList.contains('selected'))
     {
         document.getElementById('option-hddl-yes').classList.add('selected');
+        document.getElementById('option-hddl-no').classList.remove('selected');
     }
     // Blurr Effect when openvino/tensorflow is selected
     // if(document.getElementById('option-byo-tfov').classList.contains('selected'))
